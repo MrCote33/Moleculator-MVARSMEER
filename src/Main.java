62984f3a74bcabf6a3e8f3cf86675d646e295c20
José Nicolás Medina Bravo <jose.medinabravo@outlook.com>
2021-03-25 16:39:22 -0300

    Añadido archivo Interface.java

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,9 @@
-import java.awt.Dimension;
-import javax.swing.JFrame;
-
 public class Main {
-	
-	private static void CreateWindow() {
-		
-		 JFrame Window = new JFrame("Moleculator");
-		 Window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		 Window.setSize(new Dimension(854,480));
-		 Window.setLocationRelativeTo(null);
-		 Window.setVisible(true);
-		
-	}
 
 	public static void main(String[] args) {
 		
-		CreateWindow();
+		Interface Test = new Interface();
+		Test.CreateWindow();
 
 	}
 

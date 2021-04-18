87d3ca34d6b27f00447dcc05d9dca6df2b60a298
Roberto Cañete <robertoandrecane@hotmail.com>
2021-04-18 18:16:43 -0400

    Se realizo corrección al momento de mostrar el menú de selección

--- src/xyz/mvarsmeer/moleculator/Interface.java
+++ src/xyz/mvarsmeer/moleculator/Interface.java
@@ -1,44 +1,61 @@
 package xyz.mvarsmeer.moleculator;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class Interface {
 
 	public void initInterface() {
 
-		JFrame Window = new JFrame("Moleculator");
-		Window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Frame Window = new Frame("Moleculator");
+		Window.addWindowListener(new WindowListener());
 		Window.setSize(new Dimension(854, 480));
 		Window.setLocationRelativeTo(null);
-		Window.setLayout(null);
 		Window.setResizable(false);
-
-		JLabel Equipo = new JLabel("MVARSMEER", JLabel.CENTER);
-		Equipo.setBounds(750, 420, 80, 15);
-		Window.add(Equipo);
+		Window.setLayout(null);
 
 		Buttons Boton = new Buttons();
-		Boton.CrearBoton(Window, 30, 30, "Metales alcainos", "#FFC8B0");
-		Boton.CrearBoton(Window, 30, 60, "Alcalinoterreos", "#FFE6CC");
-		Boton.CrearBoton(Window, 30, 90, "Otros metales", "#FFF2CC");
-		Boton.CrearBoton(Window, 30, 120, "Metales de transicion", "#D0E882");
-		Boton.CrearBoton(Window, 30, 150, "Lantanidos", "#D1E8A2");
-		Boton.CrearBoton(Window, 30, 180, "Actinidos", "#B8E8B3");
-		Boton.CrearBoton(Window, 30, 210, "Metaloides", "#D5E8D4");
-		Boton.CrearBoton(Window, 30, 240, "No metales", "#E1D5E7");
-		Boton.CrearBoton(Window, 30, 270, "Halogenos", "#D8A4DE");
-		Boton.CrearBoton(Window, 30, 300, "Gases nobles", "#DAE8FC");
-		Boton.CrearBoton(Window, 30, 330, "Custom", "#E9E4E3");
+		Boton.CrearBoton(Window, 35, 60, "Metales alcainos", "#FFC8B0");
+		Boton.CrearBoton(Window, 35, 90, "Alcalinoterreos", "#FFE6CC");
+		Boton.CrearBoton(Window, 35, 120, "Otros metales", "#FFF2CC");
+		Boton.CrearBoton(Window, 35, 150, "Metales de transicion", "#D0E882");
+		Boton.CrearBoton(Window, 35, 180, "Lantanidos", "#D1E8A2");
+		Boton.CrearBoton(Window, 35, 210, "Actinidos", "#B8E8B3");
+		Boton.CrearBoton(Window, 35, 240, "Metaloides", "#D5E8D4");
+		Boton.CrearBoton(Window, 35, 270, "No metales", "#E1D5E7");
+		Boton.CrearBoton(Window, 35, 300, "Halogenos", "#D8A4DE");
+		Boton.CrearBoton(Window, 35, 330, "Gases nobles", "#DAE8FC");
+		Boton.CrearBoton(Window, 35, 360, "Custom", "#E9E4E3");
 
 		Canvas DrawingZone = new Canvas();
-		DrawingZone.setBounds(220, 30, 586, 380);
+		DrawingZone.setBounds(225, 60, 586, 380);
+		DrawingZone.setBackground(Color.decode("#0F0F0F"));
 		Window.add(DrawingZone);
 
 		Window.setVisible(true);
+		
+		while(true) {
+			
+			try {
+				Thread.sleep(17);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}
 
+	}
+	
+	static class WindowListener extends WindowAdapter {
+		
+		public void windowClosing(WindowEvent e) {
+			System.exit(0);
+		}
+		
 	}
 
 }

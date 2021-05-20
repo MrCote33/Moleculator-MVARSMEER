dc758cb5794544ac50458cee0141c8754487b65b
José Medina Bravo <jose.medinabravo@outlook.com>
2021-05-19 22:54:48 -0400

    Los enlaces entre Atomos toman un promedio del color entre ambos

--- src/xyz/mvarsmeer/moleculator/DrawLine.java
+++ src/xyz/mvarsmeer/moleculator/DrawLine.java
@@ -12,7 +12,7 @@ public class DrawLine {
 	DrawAtom Primero;
 	DrawAtom Segundo;
 	int EnlEfectivos;
-
+	Color Paint;
 	
 	public void setFirst(DrawAtom Actual) {
 
@@ -24,6 +24,12 @@ public class DrawLine {
 
 		this.Segundo = Actual;
 		this.Segundo.Repaint = true;
+		
+		int R = (this.Primero.Paint.getRed() + this.Segundo.Paint.getRed()) / 2;
+		int G = (this.Primero.Paint.getGreen() + this.Segundo.Paint.getGreen()) / 2;
+		int B = (this.Primero.Paint.getBlue() + this.Segundo.Paint.getBlue()) / 2;
+		
+		this.Paint = Color.decode("#"+Integer.toHexString(R)+Integer.toHexString(G)+Integer.toHexString(B));
 		this.EnlEfectivos = EnlEfectivos+1;
 
 	}
@@ -37,7 +43,7 @@ public class DrawLine {
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		
 		g2.setFont(Fuente1);
-		g2.setColor(Color.decode("#FF0000"));
+		g2.setColor(Paint);
 
 		g2.setStroke(new BasicStroke(6));
 
